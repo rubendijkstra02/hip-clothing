cb1c251d45abe021b042e314a80ac0186f4751a9
rubendijkstra02 <rubendijkstra1997@gmail.com>
2022-11-23 14:21:25 +0100

    Added navbar with routing component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import {Outlet, Link} from "react-router-dom";
+import {ReactComponent as BrandLogo} from "../../assets/crown.svg";
+import './navigation.scss'
+
+const Navigation = () => {
+    return (
+        <>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <BrandLogo className='logo'/>
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet/>
+        </>
+    )
+}
+
+export default Navigation
